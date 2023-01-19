262ba4dcdf06a601a800aef9aea3c876460a8372
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-19 20:51:28 +0100

    add type hints

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,11 @@
+from typing import Union
+
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
 
-def get_image(db: Session, image_id: int):
+def get_image(db: Session, image_id: int) -> Union[models.Image, None]:
     return db.query(models.Image).filter(models.Image.id == image_id).first()
 
 
@@ -11,7 +13,11 @@ def get_all_images(db: Session):
     return db.query(models.Image).all()
 
 
-def create_image(db: Session, image: schemas.ImageCreate):
+def get_all_images_by_title(db: Session, image_title: str) -> Union[list[models.Image], None]:
+    return db.query(models.Image).filter(models.Image.title.like(f"%{image_title}%")).all()
+
+
+def create_image(db: Session, image: schemas.ImageCreate) -> models.Image:
     db_image = models.Image(**image.dict())
     db.add(db_image)
     db.commit()
