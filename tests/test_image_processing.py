3caff63903f075fd527a16b67c8c0d4a69729a25
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-19 23:11:08 +0100

    add test for raising FileExistsError

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import pytest
 
 from app.image_processing import create_path
@@ -12,4 +14,14 @@ from app.image_processing import create_path
     ],
 )
 def test_create_path(image_title, filename, expected_path):
-    assert create_path(image_title, filename) == expected_path
+    assert create_path(image_title, filename, "image_data") == expected_path
+
+
+def test_create_path_raises_file_exists_error():
+    image_title = "test_image_title"
+    filename = "image.jpg"
+
+    with tempfile.TemporaryDirectory() as temp_dir, open(f"{temp_dir}/test_image_title.jpg", "wb") as file:
+        file.write(b"data")
+        with pytest.raises(FileExistsError):
+            create_path(image_title, filename, temp_dir)
