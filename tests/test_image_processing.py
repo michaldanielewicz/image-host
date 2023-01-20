85e82520d27236ba3a0bba17c9a405e0a690fc4c
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-20 14:11:56 +0100

    erase reduntant f-strings

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -8,9 +8,9 @@ from app.image_processing import create_path
 @pytest.mark.parametrize(
     "image_title, filename, expected_path",
     [
-        ("title", "image.png", f"image_data/title.png"),
-        ("tricky_title", "image_jpg.jpg", f"image_data/tricky_title.jpg"),
-        ("123456", "picture.gif", f"image_data/123456.gif"),
+        ("title", "image.png", "image_data/title.png"),
+        ("tricky_title", "image_jpg.jpg", "image_data/tricky_title.jpg"),
+        ("123456", "picture.gif", "image_data/123456.gif"),
     ],
 )
 def test_create_path(image_title, filename, expected_path):
