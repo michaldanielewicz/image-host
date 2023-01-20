4d66144575d355124c5ab8bcf144441932cdf1e8
michaldanielewicz <michaldanielewicz94@gmail.com>
2023-01-20 14:35:44 +0100

    add type hint for get_db()

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,7 +11,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
